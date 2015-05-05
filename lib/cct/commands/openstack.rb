1a4e1de73f31fbd6805281be5cd30ebd9eefc0b7
Vladimir Moravec <vmoravec@suse.com>
2015-05-05 23:22:55 +0200

    Refactor the openstack class and use it with openstack.user

--- lib/cct/commands/openstack.rb
+++ lib/cct/commands/openstack.rb
@@ -2,37 +2,86 @@ require "csv"
 
 module Cct
   module Commands
+    # Is included into controller node and provides method #openstack as namespace
+    # for openstack client commands
     module Openstack
+      # @return [Openstack] object that provides the openstack client commands
+      def openstack
+        @openstack ||= Openstack::Client.new(self)
+      end
+
+      # Proxy object for accessing all openstack client commands.
+      # If a new command is added, e.g. in lib/cct/commands/openstack/new_command.rb,
+      # don't forget to add it also into the #initialize method including the attr_readers
       class Client
         COMMAND = "openstack"
 
+        # @return [Openstack::Image] for accessing openstack.image subcommands
         attr_reader :image
+        # @return [Openstack::User] for accessing openstack.user subcommands
         attr_reader :user
 
+        # @param [Cct::Node] as the receiver for the openstack client
         def initialize node
           @image = Openstack::Image.new(node)
           @user = Openstack::User.new(node)
         end
       end
 
-      def openstack
-        @openstack ||= Openstack::Client.new(self)
-      end
-
-      module NodeContext
-        def self.included client_command
-          client_command.extend(ContextMethods)
+      # Parent class to inherit from for all openstack client commands
+      # @example Implementation of openstack.user subcommands
+      #
+      # class User < Command
+      #   self.command = "user" # This will set the namespace: openstack.user
+      #
+      #   # Access with `control_node.openstack.user.create("Mozart", password: "Wien")`
+      #   def create name, options={}
+      #     super do |params|
+      #       params.add :optional, password: "--password"
+      #       params.add :optional, email:    "--email"
+      #       params.add :optional, project:  "--project"
+      #       params.add :optional, enable:   "--enable",  param_type: :switch
+      #       params.add :optional, disable:  "--disable", param_type: :switch
+      #     end
+      #   end
+      # end
+      class Command
+        class << self
+          attr_accessor :command
         end
 
-        attr_reader :node, :log
+        attr_reader :node, :log, :params
 
         def initialize node
           @node = node
-          @log  = node.log
+          @log = node.log
+          @params = Params.new
         end
 
         def exec! subcommand, *params
           node.exec!(Openstack::Client::COMMAND, self.class.command, subcommand, *params)
+        end
+
+        def create name, options={}
+          yield params
+          params.extract!(options).unshift("create", name, "--format=shell")
+          OpenStruct.new(shell_parse(exec!(params.shell).output))
+        end
+
+        def delete id_or_name
+          OpenStruct.new(shell_parse(exec!("delete", id_or_name).output))
+        end
+
+        def list
+          user_row = Struct.new(:id, :name)
+          result = exec!("list", "--format=csv").output
+          csv_parse(result).map do |row|
+            user_row.new(*row)
+          end
+        end
+
+        def show id_or_name
+          OpenStruct.new(shell_parse(exec!("show", id_or_name, "--format=shell").output))
         end
 
         def csv_parse csv_data, header: false
@@ -48,15 +97,68 @@ module Cct
           end
         end
 
-        def optional arg_name, key, params, type: nil
-          return "" unless params[key]
-          return arg_name if params[key] && type == :switch
+        class Params
+          attr_reader :mandatory, :optional, :all
 
-          "#{arg_name}=#{params[key]}"
-        end
+          def initialize
+            @mandatory = []
+            @optional = []
+            @shell = []
+          end
 
-        module ContextMethods
-          attr_accessor :command
+          def add type=:mandatory, params
+            case type
+            when :mandatory
+              mandatory.push(params)
+            when :optional
+              optional.push(params)
+            else
+              raise "Type '#{type}' for parameters not allowed"
+            end
+          end
+
+          def extract! options
+            extract_mandatory(options)
+            extract_optional(options)
+            shell
+          end
+
+          private
+
+          def extract_mandatory options
+            mandatory.each do |param|
+              param_type = param.delete(:param_type)
+              param.each_pair do |key, value|
+                raise "Parameter '#{key}' is mandatory" unless options[key]
+
+                case param_type
+                when :switch
+                  shell.push(value)
+                  next
+                else
+                  shell.push("#{key}=#{options[key]}")
+                end
+              end
+            end
+          end
+
+          def extract_optional options
+            optional.each do |param|
+              param_type = param.delete(:param_type)
+              param.each_pair do |key, value|
+                next unless options[key]
+
+                case param_type
+                when :switch
+                  shell.push(value)
+                  next
+                else
+                  shell.push("#{key}=#{options[key]}")
+                end
+              end
+            end
+          end
+
         end
       end
 
--- lib/cct/commands/openstack/user.rb
+++ lib/cct/commands/openstack/user.rb
@@ -1,44 +1,19 @@
 module Cct
   module Commands
     module Openstack
-      class User
-        include NodeContext
-
+      class User < Command
         self.command = "user"
 
-        def list
-          user_row = Struct.new(:id, :name)
-          result = exec!("list", "--format csv").output
-          csv_parse(result).map do |row|
-            user_row.new(*row)
+        def create name, options={}
+          super do |params|
+            params.add :optional, password: "--password"
+            params.add :optional, email:    "--email"
+            params.add :optional, project:  "--project"
+            params.add :optional, enable:   "--enable",  param_type: :switch
+            params.add :optional, disable:  "--disable", param_type: :switch
           end
         end
 
-        def show id_or_name
-          result = exec!("show", id_or_name, "--format shell").output
-          OpenStruct.new(shell_parse(result))
-        end
-
-        def create name, options={}
-          OpenStruct.new(
-            shell_parse(
-              exec!(
-                "create",
-                name,
-                "--format=shell",
-                optional("--password", :password, options),
-                optional("--email",    :email,    options),
-                optional("--project",  :project,  options),
-                optional("--enable",   :enable,   options, type: :switch),
-                optional("--disable",  :disable,  options, type: :switch),
-              ).output
-            )
-          )
-        end
-
-        def delete id_or_name
-          exec!("delete", id_or_name)
-        end
       end
     end
   end
