d15b50717a3a357887c859db65c035eecb4418e5
Vladimir Moravec <vmoravec@suse.com>
2015-09-04 09:04:24 +0200

    Add steps for image detection

--- features/step_definitions/functional_tests/functional_tests_steps.rb
+++ features/step_definitions/functional_tests/functional_tests_steps.rb
@@ -7,6 +7,14 @@ Given(/^the package "([^"]*)" is installed on the controller node$/) do |client_
   control_node.rpm_q(client_package)
 end
 
+When(/^the proper cirros image has been detected$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the image has been cloned with name "([^"]*)"$/) do |image_name|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
 Then(/^all the tests for the package have been executed successfully$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
