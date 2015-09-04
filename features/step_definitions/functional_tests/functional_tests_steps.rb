3dba457af3c2e3614920b039e76843dc6be14513
Vladimir Moravec <vmoravec@suse.com>
2015-09-04 13:59:54 +0200

    Update the test package paths

--- features/step_definitions/functional_tests/functional_tests_steps.rb
+++ features/step_definitions/functional_tests/functional_tests_steps.rb
@@ -31,9 +31,9 @@ end
 
 Then(/^all the funtional tests for the package pass$/) do
   control_node.exec!(
-    "/var/lib/novaclient/tests/functional/setup.py testr",
+    "cd /var/lib/python-novaclient-test;  python setup.py testr",
     "OS_NOVACLIENT_EXEC_DIR" => "/usr/bin",
-    "OS_TEST_PATH" => "/var/lib/novaclient/tests/functional"
+    "OS_TEST_PATH" => "/var/lib/python-novaclient-test/functional"
   )
 end
 
