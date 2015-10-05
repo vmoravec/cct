ac1358952db00c5668de5962e4fe9bc9f26b1a8a
Vladimir Moravec <vmoravec@suse.com>
2015-10-05 10:51:19 +0200

    Update the step implementation for rabbitmq

--- features/barclamp_rabbitmq.feature
+++ features/barclamp_rabbitmq.feature
@@ -7,10 +7,9 @@ Feature: Test the AMQP Messaging service
     Given the chef role "rabbitmq-server" exists on admin node
     And the "rabbitmq" cookbook exists on the admin node
 
-  Scenario: RabbitMQ Successful deployment
+  Scenario: RabbitMQ successful deployment
     Given the barclamp proposal is using rabbitmq as messaging backend
-    When the node with rabbitmq-server role has been detected successfully
+    And the node with rabbitmq-server role has been detected successfully
     Then I can check the status of rabbitmq-server using command line
-    And I can add a new user and view the user in the users list
-    And I can change the user's role to an adminstrator
-    And I can delete the user and verify the users list
+    When I add a new user and view the user in the users list
+    And I change the user's role to an administrator
--- features/step_definitions/barclamp_rabbitmq/rabbitmq_steps.rb
+++ features/step_definitions/barclamp_rabbitmq/rabbitmq_steps.rb
@@ -1,74 +1,44 @@
 Given(/^the barclamp proposal is using rabbitmq as messaging backend$/) do
-    json_response = JSON.parse(admin_node.exec!("crowbar rabbitmq show default").output)
-    expect(json_response["attributes"]["rabbitmq"]).not_to be_empty
+  json_response = JSON.parse(admin_node.exec!("crowbar rabbitmq show default").output)
+  expect(json_response["attributes"]["rabbitmq"]).not_to be_empty
 end
 
-When(/^the node with rabbitmq-server role has been detected successfully$/) do
-    @node = nodes.find(barclamp: "rabbitmq", element: "rabbitmq-server").first
-    node_name = @node.attributes()[:name]
-    expect(node_name).not_to be_empty
+And(/^the node with rabbitmq-server role has been detected successfully$/) do
+  @nodes = nodes.find(barclamp: "rabbitmq", element: "rabbitmq-server")
+  expect(@nodes).not_to be_empty
 end
 
 Then(/^I can check the status of rabbitmq-server using command line$/) do
-    rabbitmq_status = @node.exec!("rabbitmqctl status").output.lines[1..-1] 
-    rabbitmq_pid = 0
-    rabbitmq_status.each do |line|
-        line = line[0..-2]
-        scan_list = line.scan(/\{([^\}]*)\}/)[0][0].split(',')
-        if scan_list[0] == "pid"
-            rabbitmq_pid = scan_list[1].to_i
-            break
-        end
-    end
-    expect(rabbitmq_pid).not_to equal(0)
+  status_results = @nodes.map do |node|
+    node.exec!("rabbitmqctl status", capture_error: true)
+  end
+  expect(status_results).to succeed_at_least_once
 end
 
-And(/^I can add a new user and view the user in the users list$/) do
-    @username = "rabbit"
-    @password = "crowbar"
-    @node.exec!("rabbitmqctl add_user #{@username} #{@password}")
-    user_list = @node.exec!("rabbitmqctl list_users").output.lines[1..-1]
-
-    new_user_created = false
-    user_list.each do |user|
-        new_user_info = user[0..-2].split(/\t/)
-        if new_user_info[0] == @username
-            new_user_created = true
-            break
-        end
+When(/^I add a new user and view the user in the users list$/) do
+  @username = "cucumber_rabbit"
+  @password = "cucumber_crowbar"
+  results = @nodes.map do |node|
+    list_users = node.exec!("rabbitmqctl list_users", capture_error: true)
+    if list_users.success? && list_users.output.match(@username)
+      node.exec!("rabbitmqctl delete_user #{@username}")
+      node.exec!("rabbitmqctl add_user #{@username} #{@password}")
     end
-    expect(new_user_created).to equal(true)
+    list_users
+  end
+  expect(results).to succeed_at_least_once
 end
 
-And(/^I can change the user's role to an adminstrator$/) do
-    @node.exec!("rabbitmqctl set_user_tags #{@username} administrator")
-    user_list = @node.exec!("rabbitmqctl list_users").output.lines[1..-1]
-    new_user_info = []
-    user_list.each do |user|
-        new_user_info = user[0..-2].split(/\t/)
-        if new_user_info[0] == @username 
-            break
-        end
+And(/^I change the user's role to an administrator$/) do
+  results = @nodes.map do |node|
+    result = node.exec!(
+      "rabbitmqctl set_user_tags #{@username} administrator", capture_error: true
+    )
+    if result.success?
+      user_list = node.exec!("rabbitmqctl list_users")
+      expect(user_list.output.gsub("\t", " ")).to match(/#{@username}\s\[administrator\]/)
     end
-
-    @new_user = new_user_info[0]
-    new_user_role = new_user_info[1].delete("[]")
-
-    expect(new_user_role).not_to be_empty
-    expect(new_user_role).to eq("administrator")
+    result
+  end
+  expect(results).to succeed_at_least_once
 end
-
-And(/^I can delete the user and verify the users list$/) do
-    @node.exec!("rabbitmqctl delete_user #{@username}")
-    user_list = @node.exec!("rabbitmqctl list_users").output.lines[1..-1]
-    new_user_exist = false
-    user_list.each do |user|
-        new_user_info = user[0..-2].split(/\t/)
-        if new_user_info[0] == @username
-            new_user_exist = true
-            break
-        end
-    end
-    expect(new_user_exist).to equal(false)
-end
-
