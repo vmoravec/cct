1e4a6334a61fe8af58146d5936acc3f431353c1f
Vladimir Moravec <vmoravec@suse.com>
2017-02-22 20:59:29 +0100

    Add feature for admin node backup during upgrade

--- features/support/step_helpers.rb
+++ features/support/step_helpers.rb
@@ -4,4 +4,12 @@ module StepHelpers
     step "I can establish SSH connection"
     step "I can reach the crowbar API"
   end
+
+  def verify_upgrade_landing_page
+    step 'I click the "Upgrade" link to trigger the upgrade process'
+    step 'the upgrade process is successfuly initialized'
+    step 'I click the "Check" button to trigger preliminary checks'
+    step 'all checks show successful results'
+    step 'I get the "Begin Upgrade" button enabled'
+  end
 end
