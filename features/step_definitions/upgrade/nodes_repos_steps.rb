b04927a1ad463ed797c506e7f3d36b40eaaa4435
Vladimir Moravec <vmoravec@suse.com>
2017-03-08 11:32:48 +0100

    Add nodes repos check tests

--- features/step_definitions/upgrade/nodes_repos_steps.rb
+++ features/step_definitions/upgrade/nodes_repos_steps.rb
@@ -1,7 +1,17 @@
 Given(/^I am on the page for checking nodes repos$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit('/upgrade')
+  sleep 5
+  if page.current_url.match(/database-configuration$/)
+    find_button("Next").click
+  end
+  wait_for "Show nodes' repo page", max: "10 seconds", sleep: "1 seconds" do
+    puts "Current url: #{page.current_url}"
+    break if page.current_url.match(/nodes-repositories-checks$/)
+  end
 end
 
-When(/^I click the "([^"]*)" button to verify new cloud repos on for all nodes$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I click the "([^"]*)" button to verify new cloud repos on for all nodes$/) do |btn_text|
+  check_button = find_button(btn_text)
+  check_button.click
+  expect(check_button.disabled?).to be(true)
 end
