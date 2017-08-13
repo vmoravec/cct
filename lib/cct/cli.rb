808777d85fd2e242ac6d609528a2282db4fbe627
Vladimir Moravec <vmoravec@suse.com>
2017-08-13 23:24:27 +0200

    Add more stuff into Cct::Cli

--- lib/cct/cli.rb
+++ lib/cct/cli.rb
@@ -1,17 +1,50 @@
 module Cct
   module Cli
-    def config
+
+    def safely_parse
+      yield
+    rescue OptionParser::MissingArgument,
+           OptionParser::InvalidOption => err
+      abort "Error: #{err.message}"
+    end
+
+    def command name
+      @commands ||= OpenStruct.new
+      @commands[name] = yield if block_given?
+    end
+
+    def run_command name, options
+      safely_parse { @commands[name].order! } if @commands[name]
+      send(name, options)
+    rescue NoMethodError => e
+      puts e.backtrace if options.verbose
+      abort "Unknown command '#{name}'"
+    end
+
+    # This method is in charge of starting cucumber and also setting up Cct
+    def test options
+      puts "Options given: #{options.to_h}" if options.verbose
+      Dir.chdir(Pathname.new(File.join(__dir__, "../..")).expand_path) do |cct_dir|
+        puts "Loading from #{cct_dir}"
+        env = ""#File.exist?(LOCAL_CONFIG) ? "cct_config_file=#{LOCAL_CONFIG}" : ""
+        command = "#{env} bundle exec cucumber --tags #{options.tags} #{'--verbose' if options.verbose}".strip
+        puts command
+        system(command)
+      end
+    end
+
+    def config options
       require "awesome_print"
       Cct.setup(Pathname.new(__dir__).join("..").expand_path, verbose: false)
 
       ap Cct.config.content
     end
 
-    def console
+    def console options
       require 'irb'
       require 'cct/cloud/world'
 
-      Cct.setup(Pathname.new(__dir__).join("..").expand_path, verbose: false)
+      Cct.setup(Pathname.new(File.join(__dir__, "..", "..")).expand_path, verbose: false)
       ARGV.clear
       Cct.update_logger(Cct::BaseLogger.new('console', stdout: true))
 
